131f0e024cb34ce8590af2f123a7a8038df205a2
David Björklund <david.bjorklund@gmail.com>
2016-05-24 12:01:26 +0200

    update ava, bugfix

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ export const unshift = (array, value) => [value].concat(array);
 export const splice = (array, start, deleteCount, ...items) =>
   array.slice(0, start).concat(items).concat(array.slice(start + deleteCount));
 export const set = (array, index, value) =>
-  array.slice(0, index).concat([value]).concat(array.slice(index + 1));
+  array[index] === value
+  ? array
+  : array.slice(0, index).concat([value]).concat(array.slice(index + 1));
 export default {push, pop, shift, unshift, splice, set};
--- test.js
+++ test.js
@@ -4,31 +4,35 @@ import {push, pop, unshift, shift, splice, set} from './index';
 import arrayMethods from './index';
 
 test('push()', t => {
-  t.same(push(['a', 'b', 'c'], 'd'), ['a', 'b', 'c', 'd']);
-  t.same(push(['a', 'b', 'c'], ['d']), ['a', 'b', 'c', ['d']]);
+  t.deepEqual(push(['a', 'b', 'c'], 'd'), ['a', 'b', 'c', 'd']);
+  t.deepEqual(push(['a', 'b', 'c'], ['d']), ['a', 'b', 'c', ['d']]);
 });
 
 test('unshift()', t => {
-  t.same(unshift(['a', 'b', 'c'], 'd'), ['d', 'a', 'b', 'c']);
-  t.same(unshift(['a', 'b', 'c'], ['d']), [['d'], 'a', 'b', 'c']);
+  t.deepEqual(unshift(['a', 'b', 'c'], 'd'), ['d', 'a', 'b', 'c']);
+  t.deepEqual(unshift(['a', 'b', 'c'], ['d']), [['d'], 'a', 'b', 'c']);
 });
 
 test('pop()', t => {
-  t.same(pop(['a', 'b', 'c']), ['a', 'b']);
+  t.deepEqual(pop(['a', 'b', 'c']), ['a', 'b']);
 });
 
 test('shift()', t => {
-  t.same(shift(['a', 'b', 'c']), ['b', 'c']);
+  t.deepEqual(shift(['a', 'b', 'c']), ['b', 'c']);
 });
 
 test('splice()', t => {
-  t.same(splice(['a', 'b', 'c', 'd'], 2, 1), ['a', 'b', 'd']);
-  t.same(splice(['a', 'b', 'c', 'd'], 2, 0, 'e'), ['a', 'b', 'e', 'c', 'd']);
+  t.deepEqual(splice(['a', 'b', 'c', 'd'], 2, 1), ['a', 'b', 'd']);
+  t.deepEqual(splice(['a', 'b', 'c', 'd'], 2, 0, 'e'), ['a', 'b', 'e', 'c', 'd']);
 });
 
 test('set', t => {
-  t.same(set([0, 1, 2], 0, 666), [666, 1, 2]);
-  t.same(set([], 0, [666]), [[666]]);
+  t.deepEqual(set([0, 1, 2], 0, 666), [666, 1, 2]);
+  t.deepEqual(set([], 0, [666]), [[666]]);
+  const input = [0, 1, 2];
+  const actual = set(input, 1, 1);
+  const expected = input;
+  t.is(actual, expected);
 });
 
 test('default import', t => {
