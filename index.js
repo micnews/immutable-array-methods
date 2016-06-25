b57227c5728a74da1f8f15f19d71a076a2080105
David Bjorklund <davidbjorklund@PopBook-Pro.local>
2016-06-25 17:10:51 +0200

    more whitespace

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 export const push = (array, value) => array.concat([value]);
+
 export const pop = (array) => array.slice(0, -1);
+
 export const shift = (array) => array.slice(1);
+
 export const unshift = (array, value) => [value].concat(array);
+
 export const splice = (array, start, deleteCount, ...items) =>
   array.slice(0, start).concat(items).concat(array.slice(start + deleteCount));
+
 export const set = (array, index, value) =>
   array[index] === value
   ? array
   : array.slice(0, index).concat([value]).concat(array.slice(index + 1));
+
 export const flatten = array =>
   array.some(value => Array.isArray(value))
   ? [].concat(...array)
   : array;
+
 export const map = (array, fn) => {
   let changed = false;
   const newArray = array.map((obj, index) => {
@@ -22,4 +29,5 @@ export const map = (array, fn) => {
 
   return changed ? newArray : array;
 };
+
 export default {push, pop, shift, unshift, splice, set, flatten, map};
